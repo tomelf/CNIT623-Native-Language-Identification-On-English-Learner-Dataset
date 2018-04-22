cf3ad5130e541d9f9f5e4bf4f7a3951d6b00e2ae
tomelf <tomelf2002@gmail.com>
2018-04-21 23:51:14 -0400

    Refine task3 with Pipeline, and add function words, char ngrams, word ngrams

--- task3_svm.py
+++ task3_svm.py
@@ -14,6 +14,7 @@ from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
 from sklearn.model_selection import GridSearchCV
+from sklearn.externals import joblib
 
 from util.feature.CharNGrams import CharNGrams
 from util.feature.FunctionWords import FunctionWords
@@ -41,42 +42,61 @@ def main():
     pipeline = Pipeline([
         ('union', FeatureUnion(
             transformer_list = [
-                ('funcwords', Pipeline([
+                # ('funcwords', Pipeline([
+                #         ('tfidf', TfidfVectorizer(analyzer="word", 
+                #                                   ngram_range=(1,4), 
+                #                                   lowercase=True, 
+                #                                   vocabulary=funcwords_vocabs)
+                #         ),
+                #         ('reduce_dim', TruncatedSVD()),
+                # ])),
+                # ('charngrams', Pipeline([
+                #         ('tfidf', TfidfVectorizer(analyzer="char", 
+                #                                   ngram_range=(3,3), 
+                #                                   lowercase=True, 
+                #                                   vocabulary=char_ngrams_vocabs)
+                #         ),
+                #         ('reduce_dim', TruncatedSVD()),
+                # ])),
+                ('wordngrams', Pipeline([
                         ('tfidf', TfidfVectorizer(analyzer="word", 
-                                                  ngram_range=(1,4), 
-                                                  lowercase=True, 
-                                                  vocabulary=funcwords_vocabs)
+                                                  ngram_range=(1,2), 
+                                                  lowercase=True)
                         ),
-                        ('reduce_dim', TruncatedSVD(n_components=50)),
-                ])),
-                ('charngrams', Pipeline([
-                        ('tfidf', TfidfVectorizer(analyzer="char", 
-                                                  ngram_range=(3,3), 
-                                                  lowercase=True, 
-                                                  vocabulary=char_ngrams_vocabs)
-                        ),
-                        ('reduce_dim', TruncatedSVD(n_components=50)),
+                        ('reduce_dim', TruncatedSVD()),
                 ])),
             ],
             transformer_weights={
-                'funcwords': 1,
-                'charngrams': 1,
+                # 'funcwords': 1,
+                # 'charngrams': 1,
+                'wordngrams': 1,
             },
         )),
-        ('clf', SVC()),
+        ('clf', SVC(decision_function_shape='ovo')),
     ])
     
-    param_grid = dict(union__funcwords__reduce_dim__n_components=[10, 50, 100],
-                      union__charngrams__reduce_dim__n_components=[100, 300, 500],
-                      clf__C=[20, 30])
+    param_grid = dict(#union__funcwords__reduce_dim__n_components=[10, 50, 100],
+                      #union__charngrams__reduce_dim__n_components=[50, 100, 200],
+                      union__wordngrams__reduce_dim__n_components=[50, 100, 200],
+                      union__wordngrams__tfidf__ngram_range=[(1,1), (2,2), (1,2)],
+                      clf__C=[0.1, 1, 10, 20])
+    grid_search = GridSearchCV(pipeline, param_grid=param_grid, cv=5, verbose=10, return_train_score=True)
     
-    grid_search = GridSearchCV(pipeline, param_grid=param_grid, cv=10, return_train_score=True)
     train_pred = grid_search.fit(train_data_df, train_actual).predict(train_data_df)
     test_pred = grid_search.predict(test_data_df)
+    # with open("task3_test_funcwords.txt", "w") as output:
+    # with open("task3_test_charngrams.txt", "w") as output:
+    with open("task3_test_wordngrams.txt", "w") as output:
+    # with open("task3_test_all.txt", "w") as output:
+        output.write("Training results:\n{}\n".format(classification_report(train_actual, train_pred)))
+        output.write("Testing results:\n{}\n".format(classification_report(test_actual, test_pred)))
     print(classification_report(train_actual, train_pred))
     print(classification_report(test_actual, test_pred))
     
-    pd.DataFrame(grid_search.cv_results_).to_csv("task3_grid_search.csv")
+    # pd.DataFrame(grid_search.cv_results_).to_csv("task3_grid_search_funcwords.csv")
+    # pd.DataFrame(grid_search.cv_results_).to_csv("task3_grid_search_charngrams.csv")
+    pd.DataFrame(grid_search.cv_results_).to_csv("task3_grid_search_wordngrams.csv")
+    # pd.DataFrame(grid_search.cv_results_).to_csv("task3_grid_search_all.csv")
 
 if __name__ == "__main__":
     main()
